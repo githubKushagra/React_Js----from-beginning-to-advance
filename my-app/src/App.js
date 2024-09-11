4085854d578b74a92e45d9eb040a56a7385e46e4
githubKushagra <devkussu43@gmail.com>
2024-09-11 11:19:47 +0530

    working on app.js and app.css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import myImage from './assets/raksha_bandhan2.jpg';
 import './App.css';
 
+
+var fullName = "Kushagra Varshney";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // inside this parentheses, jsx is written
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <>
+        <nav>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Cintact</li>
+        </nav>
+        <div className="container">
+            <h1>Welcome to our website</h1>
+            <h3>This is a {fullName}</h3>
+            <p>This is a paragraph</p>
+            <img src={myImage} alt="myImage" className = "myImage"/>
+        </div>
+    </>
   );
 }
 
